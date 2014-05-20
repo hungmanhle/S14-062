6574ed33d971c475c04c5c9a1a9d2e5079c1c642
TheDiesel <fifthredux@gmail.com>
2014-05-20 02:02:03 -0700

    Added comments to DBStuff.java

--- TraaceRouteTest/src/traceroutetest/DBStuff.java
+++ TraaceRouteTest/src/traceroutetest/DBStuff.java
@@ -1,7 +1,4 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package traceroutetest;
 
 import java.sql.Connection;
@@ -10,12 +7,28 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 
-/**
- *
- * @author a00826347
- */
+/*---------------------------------------------------------------------------------------
+ DBStuff.java - This Class handles SQL Connections and Transactions.
+
+ Classes:			DBStuff.java
+				
+				
+
+ Date:			May 12, 2014
+
+ By:		    Hung Le
+
+
+ Notes:
+ This class is not to be run alone. It is to be used within other applications.
+
+ Generate the class file and run it as follows:
+ javac DBStuff.java
+	
+ ---------------------------------------------------------------------------------------*/
 public class DBStuff {
 
+	// Replace this string with a string leading to database.
     public static final String URL = "jdbc:mysql://localhost:3306/predicative?user=hank&password=poop1234";
     private Connection connection;
     
@@ -24,15 +37,14 @@ public class DBStuff {
         connection = null;
     }
     
+	// Open SQL Connection
     public void openConnection() throws SQLException 
     {
-       
-       
-            connection = DriverManager.getConnection(URL);
-            System.out.println("MySql Connection established"); 
-            
+        connection = DriverManager.getConnection(URL);
+		System.out.println("MySql Connection established"); 
     }
 
+	// Close SQL Connection
     public void closeConnection() 
     {
         System.out.println("Releasing all open resources ...");
@@ -46,6 +58,12 @@ public class DBStuff {
         }
     }
 
+	/*
+		insertNodePosition.
+		This method will insert a node given by the android application "gateway locator"
+		
+		It inserts this data into Table 1. AKA the GPS coordinates Table.
+	*/
     public boolean insertNodePosition(String IP, String Latitude, String Longitude) 
     {
 		try{
@@ -54,7 +72,7 @@ public class DBStuff {
 
 			connection.setAutoCommit(false);
 				
-			//HUNG CHANGE NAMES HERRE
+			// Change insert statement to reflect database structure.
 			mysqlStatement.executeUpdate("INSERT INTO test1 (ipAddress, latCoord, longCoord) VALUES ('" + IP + "','" + Latitude + "','" + Longitude + "')");
 			connection.commit();
 
@@ -69,12 +87,20 @@ public class DBStuff {
 		
     }
     
+	/*
+		insertNodeEdge.
+		
+		This method will insert a pair of nodes (an edge) into Table 2, the nodelist.
+		This also stores average hopTime between nodes, however this information is not
+		currently in use.
+	*/
 	public boolean insertNodeEdge(String node1, String node2, int hopTime){
 		try{
 			openConnection();
 			Statement mysqlStatement = connection.createStatement();
 			connection.setAutoCommit(false);
 			
+			// Change this to reflect database structure.
 			mysqlStatement.executeUpdate("INSERT INTO nodelist (nodeA, nodeB, hopTime) VALUES ('" + node1 + "','" + node2 + "','" + hopTime + "')");
 			connection.commit();
 			connection.setAutoCommit(true);
@@ -86,7 +112,11 @@ public class DBStuff {
 		}
 	}
 	
-	
+	/*
+		insertNetgraph.
+		
+		This method inserts a whole TRgraph of nodes.
+	*/
     public boolean insertNetgraph(TRgraph graph)
     {
 		for (int i = 0; i < (graph.getNodeCount()-1); i++)
@@ -94,7 +124,8 @@ public class DBStuff {
 			String node1   = graph.getEdge(i).getNode1();
 			String node2   = graph.getEdge(i).getNode2();
 			int    hopTime = graph.getEdge(i).getHopTime();
-			//HUNG CHANGE NAMES HERRE
+			
+			// insertNodeEdge for all the edges.
 			if(!insertNodeEdge(node1,node2,hopTime)){
 				//System.out.println("A row with PK {"+node1+" , "+node2+"} already existed...");
 			}
